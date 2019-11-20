b6c0d175e2076807a810da6ff8003d8b6829da36
Calvin Smith <cjsmith@cs.wisc.edu>
2019-11-19 21:35:00 -0600

    changes given by theo

--- analysis/ensemble/__init__.py
+++ analysis/ensemble/__init__.py
@@ -2,22 +2,6 @@ import numpy as np
 from math import ceil, exp, log
 from .ensemble import Ensemble, CLASSIFICATION_THRESHOLD, LEARNING_RATE, ACCURACY_THRESHOLD, CLASS_RATIO
 
-# normalization utility
-def normalize(*args):
-    total = sum(args)
-    return [arg / total for arg in args]
-
-# convert to prob
-def to_prob(pos_value):
-    return exp(pos_value) / (exp(pos_value) + exp(-pos_value))
-
-# summand term to keep entropy well-defined
-def entropy_summand(p):
-    if p <= 0.0: 
-        print(f"WARNING: Non-positive probability of {p}")
-        return 0.0
-    else: return (-1 * p * log2(p))
-
 # DISJUNCTION
 class Disjunction(Ensemble):
     def __init__(self, motifs, accuracy_smoothing=1):
@@ -81,45 +65,34 @@ class WeightedVote(Ensemble):
         super().__init__(motifs)
         # set up weights and class ratio
         self._class_ratio = CLASS_RATIO
-        self._weights = np.ones(len(self._motif_map))
+        self._alpha = np.ones(len(self._motif_map))
         # and motif accuracies
-        self._accuracies = np.array([motif.size for motif in self._motif_map]) / self.size
+        acc = np.array([motif.size for motif in self._motif_map]) / self.size
+        self._w_c = np.log((1 - acc - self._class_ratio) / (acc + self._class_ratio))
 
     def update(self, value, truth, learning_rate=None):
         # only do updates if the truth is good
         if truth != True:
             return None
         
+        # set the learning rate if it's passed in
         if learning_rate is None:
             learning_rate = LEARNING_RATE
 
-        # otherwise, we're doing multiplicative updates to the weights based on who agrees
-        value_index = self._value_map.index(value)
-        agreement = self._inclusion[value_index,]
-        updates = np.where(
-            agreement >= 0.5,
-            np.ones_like(agreement) * (1 + learning_rate),
-            np.ones_like(agreement) * (1 - learning_rate)
-        )
+        # multiplicative updates to alpha
+        v_i = self._value_map.index(value)
+        m_i = (self._inclusion[v_i,] * 2) - 1
 
-        # do the update
-        self._weights *= updates
-
-        m, M = np.argmin(self._weights), np.argmax(self._weights)
-        print(self._weights[m], self._weights[M])
-        print(self._accuracies[m], self._accuracies[M])
+        self._alpha = self._alpha * np.exp(-1 * learning_rate * m_i)
 
     def probabilities(self):
-        fnr = 1 - np.exp(self._weights) / (np.exp(self._weights) + np.exp(-1 * self._weights))
-        accuracies = np.clip(1 - (self._accuracies - self._class_ratio + 2 * fnr), 0.001, 0.999)
-        p_true = np.exp(self._inclusion @ np.transpose(np.log(accuracies)))
-        p_false = np.exp(self._inclusion @ np.transpose(np.log(1 - accuracies)))
+        w_i = self._w_c + 2 * self._alpha * self._class_ratio
 
-        yes = self._inclusion @ np.transpose(np.exp(self._weights))
-        no = (1 - self._inclusion) @ np.transpose(np.exp(-1 * self._weights))
-        trial = (yes + no) / np.sum(np.exp(self._weights))
+        s_plus = self._inclusion @ np.transpose(np.exp(w_i))
+        s_minus = (1 - self._inclusion) @ np.transpose(np.exp(-1 * w_i))
+        Z = np.sum(np.exp(w_i))
 
-        return trial, 1 - trial
+        return (s_plus + s_minus) / Z, 1 - ((s_plus + s_minus) / Z)
 
     def classified(self):
         score_for, score_against = self.probabilities()
@@ -133,15 +106,6 @@ class WeightedVote(Ensemble):
             np.zeros_like(p_true)
         )
         return self._value_map[np.argmax(entropy)]
-
-    # def max_entropy(self, domain):
-    #     score_for, score_against = self.scores()
-    #     entropy = np.where(
-    #         self.to_row(domain) == 1,
-    #         np.abs(score_for - score_against),
-    #         np.ones_like(score_for) * np.infty
-    #     )
-    #     return self._value_map[np.argmin(entropy)]
 
 ENSEMBLES = {
     'disjunction' : Disjunction,
